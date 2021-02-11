b8d75f1155f980c215694263a44ede712f3ac5a8
Alexander Shukan <avshukan@gmail.com>
2021-02-11 16:57:51 +1100

    Add Compute Results

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+// https://telegram.me/prefVoteBot?start=20
 // https://www.npmjs.com/package/telegraf
 // https://habr.com/ru/post/483660/
 // https://techrocks.ru/2017/10/07/how-to-build-a-simple-telegram-bot-on-node-js/
 // https://metanit.com/web/nodejs/8.5.php
+// Keyboards https://github.com/telegraf/telegraf/blob/master/docs/examples/keyboard-bot.js
 
 'use strict';
 require('dotenv').config();
-const { Telegraf, Markup } = require('telegraf');
+const { Telegraf, Context, Markup } = require('telegraf');
 const {
   TELEGRAM_TOKEN,
   MYSQL_HOSTNAME,
@@ -31,15 +33,35 @@ if (TELEGRAM_TOKEN === undefined) {
   throw new Error('TELEGRAM_TOKEN must be provided!');
 }
 
+// class VoteContext extends Context {
+//   constructor(update, telegram, options) {
+//     console.log('Creating context for %j', update)
+//     super(update, telegram, options)
+//     this.userId = update.message.from.id;
+//     this.x = 'xxx';
+//     console.log('this', this);
+//   }
+
+//   reply(...args) {
+//     // console.log('reply called with args: %j', args)
+//     console.log('reply this', this)
+//     return super.reply(...args)
+//   }
+// }
+
+// const bot = new Telegraf(TELEGRAM_TOKEN, { contextType: VoteContext })
 const bot = new Telegraf(TELEGRAM_TOKEN);
 
 bot.use(Telegraf.log());
 
 let state = {};
 
+bot.start(start);
+
 bot.command('new', commandNew);
 bot.hears('✔️ Done', hearsDone);
 bot.hears('❌ Cancel', hearsCancel);
+bot.hears('👁 Results', hearsResults);
 bot.on('text', onText);
 
 function commandNew(context) {
@@ -54,6 +76,42 @@ function commandNew(context) {
     .oneTime()
     .resize(),
   );
+}
+
+async function start(context) {
+  const userId = context.message.from.id;
+  if (!state[userId])
+    state[userId] = { id: userId };
+  state[userId].command = 'vote';
+  state[userId].subCommand = '';
+  console.log('start context', context);
+  const pollId = context.startPayload;
+  console.log('context.startPayload', context.startPayload);
+  console.log('pollId', pollId);
+  const questionSQL = 'SELECT * FROM `prefvotebot_questions` WHERE `Id` = ?';
+  const [questionRow] = await promisePool.execute(questionSQL, [pollId]);
+  console.log('questionRow', questionRow);
+  const optioinsSQL = 'SELECT * FROM `prefvotebot_options` WHERE `QuestionId` = ?';
+  const dataSQL = [pollId];
+  const [optionsRows] = await promisePool.execute(optioinsSQL, dataSQL);
+  console.log('optionsRows', optionsRows);
+
+
+  state[userId].pollId = pollId;
+  state[userId].name = questionRow[0].Name;
+  state[userId].text = questionRow[0].Text;
+  state[userId].options = optionsRows;
+  state[userId].optionsSelected = [];
+  state[userId].mid = [];
+
+  const text = `${state[userId].name}\n${state[userId].text}`;
+  const buttons = optionsRows.map(option => option.Name);
+  const voteMessageId = await context.replyWithMarkdown(text, Markup
+    .keyboard([...buttons.map(button => [button]), ['❌ Cancel']])
+    .oneTime()
+    .resize(),
+  );
+  state[userId].voteMessageId = voteMessageId;
 }
 
 async function hearsDone(context) {
@@ -73,33 +131,188 @@ async function hearsDone(context) {
   const optionResult = await promisePool.query(optionSql, [optionValues]);
   console.log('optionResult', optionResult);
 
-  let text = `Формирование вопроса завершено!\n${state[userId].name}\n${state[userId].text}`;
-  state[userId].options.forEach(element => {
-    text += `\n${element}`;
-  });
-  context.reply(text);
+
+  const text = `Опрос **${state[userId].name}** сформирован!\n'
+    + 'Принять участие можно по ссылке\n'
+    + 'https://telegram.me/prefVoteBot?start=${questionId}`;
+  // state[userId].options.forEach(element => {
+  //   text += `\n${element}`;
+  // });
+  context.replyWithMarkdown(text);
 }
+
+
+
+async function hearsResults(context) {
+  console.log('state', state);
+  const userId = context.message.from.id;
+  if (!state[userId])
+    state[userId] = { id: userId };
+  state[userId].command = null;
+
+const QuestionId = 30;
+const optsql = `SELECT * FROM prefvotebot_options WHERE QuestionId = ?`;
+const optdata = [QuestionId];
+const [optrows] = await promisePool.query(optsql, optdata);
+const optionsList = optrows.map(item => item.Id);
+// const optionsList = [36, 37];
+
+  const sql = `SELECT
+	r1.OptionId Option1,
+	r2.OptionId Option2,
+	SUM(CASE WHEN r1.Rank < r2.Rank THEN 1 ELSE 0 END) K
+FROM (
+	SELECT *
+	FROM prefvotebot_ranks
+	WHERE QuestionId = ?
+) r1
+INNER JOIN (
+	SELECT *
+	FROM prefvotebot_ranks
+	WHERE QuestionId = ?
+) r2
+ON r1.User = r2.User
+GROUP BY
+	r1.OptionId,
+	r2.OptionId`;
+  const data = [QuestionId, QuestionId];
+  const [rows, fields] = await promisePool.query(sql, data);
+  console.log('result rows', rows)
+  context.replyWithMarkdown('result');
+
+  const d = [];
+  const p = [];
+  optionsList.forEach(option1 => {
+    d[option1] = [];
+    optionsList.forEach(option2 => {
+      d[option1][option2] = 0;
+    });
+  });
+  optionsList.forEach(option1 => {
+    p[option1] = [];
+    optionsList.forEach(option2 => {
+      p[option1][option2] = 0;
+    });
+  });
+
+  console.log('d', d);
+  rows.forEach(row => d[row.Option1][row.Option2] = parseInt(row.K));
+  console.log('d', d);
+
+  console.log('p start', p);
+
+  for (let i = 0; i < optionsList.length; i++) {
+    for (let j = 0; j < optionsList.length; j++) {
+      if (i !== j) {
+        if (d[optionsList[i]][optionsList[j]] > d[optionsList[j]][optionsList[i]]) {
+          p[optionsList[i]][optionsList[j]] = d[optionsList[i]][optionsList[j]];
+        } else {
+          p[optionsList[i]][optionsList[j]] = 0;
+        }
+      }
+    }
+  }
+
+  console.log('p continue', p);
+
+  for (let i = 0; i < optionsList.length; i++) {
+    for (let j = 0; j < optionsList.length; j++) {
+      if (i !== j) {
+        for (let k = 0; k < optionsList.length; k++) {
+          if (i !== k && j !== k) {
+            p[optionsList[j]][optionsList[k]] = Math.max(
+              p[optionsList[j]][optionsList[k]],
+              Math.min(
+                p[optionsList[j]][optionsList[i]],
+                p[optionsList[i]][optionsList[k]]
+              )
+            );
+          }
+        }
+      }
+    }
+  }
+
+  console.log('p end', p);
+
+  const optionsMarks = [];
+  for (let i = 0; i < optionsList.length; i++) {
+    optionsMarks[optionsList[i]] = 0;
+    for (let j = 0; j < optionsList.length; j++) {
+      if (p[optionsList[i]][optionsList[j]] > p[optionsList[j]][optionsList[i]]) {
+        optionsMarks[optionsList[i]] += 2;
+      } else if (p[optionsList[i]][optionsList[j]] > p[optionsList[j]][optionsList[i]]) {
+        optionsMarks[optionsList[i]] += 1;
+      }
+    }
+  }
+  console.log('optionsMarks', optionsMarks);
+
+  const optionsRating = optionsList.map(option => {
+    const result = {
+      id: option,
+      mark: optionsMarks[option],
+      place: optionsMarks.filter(item => (item > optionsMarks[option])).length,
+      count: optionsMarks.filter(item => (item === optionsMarks[option])).length,
+    };
+    return result;
+  })
+
+  console.log('optionsRating', optionsRating);
+
+  const optionsResult = optionsRating
+    .sort((item1, item2) => (item1.place < item2.place))
+    .map(item => {
+      const place = (item.place + 1);
+      const name = optrows.filter(row => row.Id === item.id)[0].Name;
+      return `${place}. ${name}`;
+    })
+  let replyText = '';
+  optionsResult.forEach(option => replyText += `${option}\n`);
+  context.replyWithMarkdown(replyText, Markup
+    .keyboard([['/new']])
+    .oneTime()
+    .resize(),
+  );
+
+}
+
+
 
 function hearsCancel(context) {
   console.log('state', state);
   const userId = context.message.from.id;
   if (!state[userId])
     state[userId] = { id: userId };
+  let replyText;
+  switch (state[userId].command) {
+    case 'new':
+      replyText = 'Создание опроса отменено';
+      break;
+    case 'vote':
+      replyText = 'Участие в опросе прервано';
+      break;
+  };
   state[userId].command = '';
-  context.replyWithMarkdown('Создание опроса отменено', Markup
+  context.replyWithMarkdown(replyText, Markup
     .keyboard([['/new']])
     .oneTime()
     .resize(),
   );
 }
 
-function onText(context) {
+
+
+async function onText(context) {
+  console.log('onText start state', state);
   const userId = context.message.from.id;
   const text = context.message.text;
 
   if (!state[userId])
     state[userId] = { id: userId };
   state[userId].index = 0;
+
+  console.log('onText middle state', state);
 
   if (state[userId].command === 'new') {
     switch (state[userId].subCommand) {
@@ -132,40 +345,70 @@ function onText(context) {
         break;
     }
   }
-  console.log('state', state);
+  if (state[userId].command === 'vote') {
+    console.log('onText state options', state[userId].options);
+    const optionIndex = state[userId].options.findIndex(option => option.Name === text);
+    console.log('onText optionIndex', optionIndex);
+    if (optionIndex === -1) {
+      const text = `Простите, такого значения нет в списке вариантов\n** ${state[userId].name} **\n${state[userId].text}`;
+      const buttons = state[userId].options.map(option => [option.Name]);
+      const mid = await context.replyWithMarkdown(text, Markup
+        .keyboard([...buttons.map(button => [button]), ['❌ Cancel']])
+        .oneTime()
+        .resize(),
+      );
+      state[userId].mid.push(mid);
+    } else {
+      const selectedOption = state[userId].options.splice(optionIndex, 1);
+      state[userId].optionsSelected.push(...selectedOption);
+      if (state[userId].options.length > 1) {
+        const buttons = state[userId].options.map(option => option.Name);
+        console.log('id', context.message.message_id);
+        await context.deleteMessage(context.message.message_id);
+        const del = await context.deleteMessage(state[userId].voteMessageId.message_id);
+        console.log('del', del);
+        let text = `${state[userId].name}\n${state[userId].text}\nВы уже выбрали:`;
+        state[userId].optionsSelected.forEach((option, index) => {
+          text += `\n${index + 1}. ${option.Name}`;
+        });
+        const mid = await context.replyWithMarkdown(text, Markup
+          .keyboard([...buttons.map(button => [button]), ['❌ Cancel']])
+          .oneTime()
+          .resize(),
+        );
+        state[userId].voteMessageId = mid;
+        state[userId].mid.push(mid);
+      } else {
+        const selectedOption = state[userId].options.pop();
+        console.log('last option', selectedOption);
+        state[userId].optionsSelected.push(selectedOption);
+        console.log('state[userId].optionsSelected all', state[userId].optionsSelected);
+        const saveData = state[userId].optionsSelected.map((option, index) => {
+          return [option.QuestionId, option.Id, index + 1, userId];
+        });
+        console.log('saveData', saveData);
+
+        const saveSql = 'INSERT INTO `prefvotebot_ranks` (`QuestionId`, `OptionId`, `Rank`, `User`) VALUES ?';
+        const saveResult = await promisePool.query(saveSql, [saveData]);
+        console.log('saveResult', saveResult);
+        let text = `Вы завершили опрос * ${state[userId].name} * \nВаш выбор:`;
+        state[userId].optionsSelected.forEach((option, index) => {
+          text += `\n${index + 1}. ${option.Name}`;
+        });
+        context.replyWithMarkdown(text, Markup
+          .keyboard([['👁 Results']])
+          .oneTime()
+          .resize(),
+        );
+
+      }
+    }
+
+  }
+  console.log('onText state', state);
+  console.log('onText state mid', state[userId].mid);
+  console.log('onText state optionsSelected', state[userId].optionsSelected);
 }
-
-// bot.command('custom', async (ctx) => {
-//   return await ctx.reply('Custom buttons keyboard', Markup
-//     .keyboard([
-//       ['🔍 Search', '😎 Popular'], // Row1 with 2 buttons
-//       ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-//       ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 buttons
-//     ])
-//     .oneTime()
-//     .resize()
-//   )
-// })
-
-// bot.hears('🔍 Search', ctx => ctx.reply('Yay!'))
-// bot.hears('📢 Ads', ctx => ctx.reply('Free hugs. Call now!'))
-
-// bot.command('special', (ctx) => {
-//   return ctx.reply(
-//     'Special buttons keyboard',
-//     Markup.keyboard([
-//       Markup.button.contactRequest('Send contact'),
-//       Markup.button.locationRequest('Send location')
-//     ]).resize()
-//   )
-// })
-
-// bot.command('simple', (ctx) => {
-//   return ctx.replyWithHTML(
-//     '<b>Coke</b> or <i>Pepsi?</i>',
-//     Markup.keyboard(['Coke', 'Pepsi'])
-//   )
-// })
 
 // bot.command('inline', (ctx) => {
 //   return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', {
