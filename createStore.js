543bd64ddabb1af5bfcb341151a769841f8f86a2
Alexander Shukan <avshukan@gmail.com>
2021-02-15 17:10:36 +1100

    Add createStore.js and botReducer.js

---
+++ createStore.js
@@ -0,0 +1,21 @@
+function createStore(storeReducer, storeState = {}) {
+  console.log('createStore', this);
+  let state = storeState;
+
+  function dispatch(action) {
+    console.log('dispatch action this', this);
+    console.log('dispatch state before', state);
+    const { updatedState, handler } = storeReducer(state, action);
+    state = updatedState;
+    console.log('dispatch state after', state);
+    return handler;
+  };
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, getState };
+}
+
+module.exports = createStore;
