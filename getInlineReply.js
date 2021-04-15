2317685346ee0da3df9810593245271f7a0ee35a
Alexander Shukan <avshukan@gmail.com>
2021-04-15 17:48:35 +1100

    feature: add wrap before HINT button

--- getInlineReply.js
+++ getInlineReply.js
@@ -1,16 +1,16 @@
 const { Markup } = require('telegraf');
+const { BUTTONS } = require('./button_types');
+
+const COLUMNS = 3;
 
 function getInlineReply(buttons) {
   if (!Array.isArray(buttons) || buttons.length === 0) return { parse_mode: 'HTML' };
-  // return ctx.reply(
-  //   'Keyboard wrap',
-  //   Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six', 'seven'], {
-  //     columns: parseInt(ctx.match[1])
-  //   })
-  // )
   const inlineReply = Markup.inlineKeyboard(
     buttons.map((button) => Markup.button.callback(button, button)),
-    { columns: 3 },
+    {
+      // wrap: (btn, index, currentRow) => (buttons[index] === BUTTONS.HINT) || !(index % COLUMNS),
+      wrap: (btn, index) => (buttons[index] === BUTTONS.HINT) || !(index % COLUMNS),
+    },
   );
   inlineReply.parse_mode = 'HTML';
   return inlineReply;
